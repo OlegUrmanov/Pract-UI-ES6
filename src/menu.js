f7d2c047973824fbf4ac612a6a1c4e37f5901945
OlegUrmanov <43071954+OlegUrmanov@users.noreply.github.com>
2018-11-25 17:00:34 +0300

    Add files via upload

--- src/menu.js
+++ src/menu.js
@@ -1,62 +1,104 @@
-class Menu {
-
-  constructor(options) {
-    this.elem;
-    this.options = options;
+((d => {
+  function Menu(options) {
+    this._element = options.element;
+    this._title = options.title;
+    this._items = options.items;
   }
-
-  getElem() {
-    if (!this.elem) {
-      this.render();
-    }
-    return this.elem;
-  }
-
-  render() {
-    this.elem = document.createElement('div');
-    this.elem.className = "menu";
-
-    let titleElem = document.createElement('span');
-    this.elem.appendChild(titleElem);
-    titleElem.className = "title";
-    titleElem.textContent = this.options.title;
-
-    this.elem.onmousedown = function() {
-      return false;
-    };
-
-    let tg = () => this.toggle();
-    this.elem.onclick = function(event) {
-      if (event.target.closest('.title')) {
-        tg();
+  Object.defineProperties(Menu.prototype, {
+    'title': {
+      get() {
+        return this._title;
+      },
+      set(title) {
+        this._title = title;
       }
-    }
-  }
+    },
+    'items': {
+      get() {
+        return this._items;
+      },
+      set(items) {
+        this._items = items;
+      }
+    },
+    'init': {
+      value() {
+        const title = d.createElement('h4');
+        const list = d.createElement('ul');
+        list.className = 'menu';
+        this._element.innerHTML = '';
+        title.textContent = this._title;
+        this._items.forEach(e => {
+          const item = d.createElement('li');
+          item.textContent = e;
+          list.appendChild(item);
+        });
+        this._element.appendChild(title);
+        this._element.appendChild(list);
+      }
+    },
+    'add': {
+      value(elem) {
+        this._items = this._items.concat(elem);
+        const item = d.createElement('li');
+        item.textContent = elem;
+        this._element.querySelector('.menu').appendChild(item);
+      }
+    },
+    'remove': {
+      value() {
+        if (this._element.querySelector('.menu').children.length){
+          this._element.querySelector('.menu').children[this._element.querySelector('.menu').children.length-1].remove();
+          this._items.pop();
+        }
+      }
+    },
+    'toggle': {
+      value() {
+        this._element.querySelector('.menu').classList.toggle('hidden');
+      }
+    },
+    'open': {
+      value() {
+        if (this._element.querySelector('.menu').classList.contains('hidden'))
+          this._element.querySelector('.menu').classList.remove('hidden');
+      }
+    },
+    'close': {
+      value() {
+        if (!this._element.querySelector('.menu').classList.contains('hidden'))
+          this._element.querySelector('.menu').classList.add('hidden');
+      }
+    },
+  });
 
-  renderItems() {
-    let items = this.options.items || [];
-    let list = document.createElement('ul');
-    for (let it in items) {
-      let li = document.createElement('li');
-      li.textContent = items[it];
-      list.appendChild(li);
-    }
-    this.elem.appendChild(list);
-  }
+  const menu = new Menu({
+    element: d.getElementById('test'),
+    title: 'Сладости',
+    items: [
+      "Торт",
+      "Пончик",
+      "Пирожное",
+      "Шоколадка",
+      "Мороженое",
+    ]
+  });
+  menu.init();
+  d.body.addEventListener('click',e => {
+    e.target.id === 'menu-toggle' ? menu.toggle() :
+    e.target.id === 'menu-open' ? menu.open() :
+    e.target.id === 'menu-close' ? menu.close() :
+    e.target.id === 'remove-item' ? menu.remove() :
+    (e.target.id === 'add-item' &&
+     d.getElementById('field-adding').value.trim() !== '') ?
+      (menu.add(d.getElementById('field-adding').value.trim()),
+       d.getElementById('field-adding').value = '') : false;
+  });
 
-  open() {
-    if (!this.elem.querySelector('ul')) {
-      this.renderItems();
-    }
-    this.elem.classList.add('open');
-  }
-
-  close() {
-    this.elem.classList.remove('open');
-  }
-
-  toggle() {
-    this.elem.classList.contains('open') ? this.close() : this.open();
-  }
-
-}
+  // console.log(menu);
+  // console.log(Object.entries(menu));
+  // console.log(Object.keys(menu));
+  // console.log(Object.getOwnPropertyNames(menu));
+  // console.log(Object.getOwnPropertySymbols(menu));
+  // console.log(Object.isExtensible(menu));
+}))(document);
